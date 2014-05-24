bd56cea9b346d18e806e1bdc64056c8554a9fc2e
Kamil Sorokosz <ksorokosz@wp.pl>
2014-05-24 11:07:36 +0200

    Memory Allocator - init state

---
+++ Cworking_Memory_Allocator/Headers/CW_Memory_Allocator_Main.h
@@ -0,0 +1,16 @@
+/**
+ * @file CW_Memory_Allocator_Main.h
+ *
+ * @date 22-05-2014
+ * @author Kamil Sorokosz
+ * @brief File contains main project headers
+ */
+
+#ifndef _CW_MEMORY_ALLOCATOR_MAIN_H__
+#define _CW_MEMORY_ALLOCATOR_MAIN_H__
+
+#include "CW_Memory_Allocator_Definitions.h"
+#include "CW_Memory_Allocator_Types.h"
+#include "CW_Memory_Allocator_Definitions.h"
+
+#endif /* _CW_MEMORY_ALLOCATOR_MAIN_H__ */
